cef1eff23d1d95a7f73735eff35e549520290730
Santiago Martinez <martinezsantiagowd@gmail.com>
2020-04-10 14:04:10 -0500

    Agregadas alertas y nombre.

--- js/main.js
+++ js/main.js
@@ -1,99 +1,113 @@
-var context = document.getElementById('puzzle').getContext('2d');
+if (!localStorage.getItem("nombre_usuario_puzzle")) {
 
-var img = new Image();
-img.src = './img/rompecabezas.png';
-img.addEventListener('load', drawTiles, false);
+} else {
+  var context = document.getElementById('puzzle').getContext('2d');
 
-var boardSize = document.getElementById('puzzle').width;
-var tileCount = document.getElementById('scale').value;
+  var img = new Image();
+  img.src = './img/rompecabezas.png';
+  img.addEventListener('load', drawTiles, false);
 
-var tileSize = boardSize / tileCount;
+  var boardSize = document.getElementById('puzzle').width;
+  var tileCount = document.getElementById('scale').value;
 
-var clickLoc = new Object;
-clickLoc.x = 0;
-clickLoc.y = 0;
+  var tileSize = boardSize / tileCount;
 
-var emptyLoc = new Object;
-emptyLoc.x = 0;
-emptyLoc.y = 0;
+  var clickLoc = new Object;
+  clickLoc.x = 0;
+  clickLoc.y = 0;
 
-var solved = false;
+  var emptyLoc = new Object;
+  emptyLoc.x = 0;
+  emptyLoc.y = 0;
 
-var boardParts;
-setBoard();
+  var solved = false;
 
-document.getElementById('scale').onchange = function() {
-  tileCount = this.value;
-  tileSize = boardSize / tileCount;
+  var boardParts;
   setBoard();
-  drawTiles();
-};
 
-document.getElementById('puzzle').onclick = function(e) {
-  clickLoc.x = Math.floor((e.pageX - this.offsetLeft) / tileSize);
-  clickLoc.y = Math.floor((e.pageY - this.offsetTop) / tileSize);
-  if (distance(clickLoc.x, clickLoc.y, emptyLoc.x, emptyLoc.y) == 1) {
-    slideTile(emptyLoc, clickLoc);
+  document.getElementById('scale').onchange = function () {
+    tileCount = this.value;
+    tileSize = boardSize / tileCount;
+    setBoard();
     drawTiles();
-  }
-  if (solved) {
-    setTimeout(function() {alert("¡Lo has resuelto!");}, 500);
-  }
-};
+  };
 
-function setBoard() {
-  boardParts = new Array(tileCount);
-  for (var i = 0; i < tileCount; ++i) {
-    boardParts[i] = new Array(tileCount);
-    for (var j = 0; j < tileCount; ++j) {
-      boardParts[i][j] = new Object;
-      boardParts[i][j].x = (tileCount - 1) - i;
-      boardParts[i][j].y = (tileCount - 1) - j;
+  document.getElementById('puzzle').onclick = function (e) {
+    clickLoc.x = Math.floor((e.pageX - this.offsetLeft) / tileSize);
+    clickLoc.y = Math.floor((e.pageY - this.offsetTop) / tileSize);
+    if (distance(clickLoc.x, clickLoc.y, emptyLoc.x, emptyLoc.y) == 1) {
+      slideTile(emptyLoc, clickLoc);
+      drawTiles();
     }
-  }
-  emptyLoc.x = boardParts[tileCount - 1][tileCount - 1].x;
-  emptyLoc.y = boardParts[tileCount - 1][tileCount - 1].y;
-  solved = false;
-}
+    if (solved) {
+      Swal.fire({
+        title: '¡Muy bien!, lo has resuelto',
+        text: 'Pulsa el botón para reiniciar.',
+        icon: 'success',
+        confirmButtonText: 'Reiniciar'
+      }).then((result) => {
+        if (result.value) {
+          location.reload(true)
+        }
+      })
+    }
+  };
 
-function drawTiles() {
-  context.clearRect ( 0 , 0 , boardSize , boardSize );
-  for (var i = 0; i < tileCount; ++i) {
-    for (var j = 0; j < tileCount; ++j) {
-      var x = boardParts[i][j].x;
-      var y = boardParts[i][j].y;
-      if(i != emptyLoc.x || j != emptyLoc.y || solved == true) {
-        context.drawImage(img, x * tileSize, y * tileSize, tileSize, tileSize,
+  function setBoard() {
+    boardParts = new Array(tileCount);
+    for (var i = 0; i < tileCount; ++i) {
+      boardParts[i] = new Array(tileCount);
+      for (var j = 0; j < tileCount; ++j) {
+        boardParts[i][j] = new Object;
+        boardParts[i][j].x = (tileCount - 1) - i;
+        boardParts[i][j].y = (tileCount - 1) - j;
+      }
+    }
+    emptyLoc.x = boardParts[tileCount - 1][tileCount - 1].x;
+    emptyLoc.y = boardParts[tileCount - 1][tileCount - 1].y;
+    solved = false;
+  }
+
+  function drawTiles() {
+    context.clearRect(0, 0, boardSize, boardSize);
+    for (var i = 0; i < tileCount; ++i) {
+      for (var j = 0; j < tileCount; ++j) {
+        var x = boardParts[i][j].x;
+        var y = boardParts[i][j].y;
+        if (i != emptyLoc.x || j != emptyLoc.y || solved == true) {
+          context.drawImage(img, x * tileSize, y * tileSize, tileSize, tileSize,
             i * tileSize, j * tileSize, tileSize, tileSize);
+        }
       }
     }
   }
-}
 
-function distance(x1, y1, x2, y2) {
-  return Math.abs(x1 - x2) + Math.abs(y1 - y2);
-}
-
-function slideTile(toLoc, fromLoc) {
-  if (!solved) {
-    boardParts[toLoc.x][toLoc.y].x = boardParts[fromLoc.x][fromLoc.y].x;
-    boardParts[toLoc.x][toLoc.y].y = boardParts[fromLoc.x][fromLoc.y].y;
-    boardParts[fromLoc.x][fromLoc.y].x = tileCount - 1;
-    boardParts[fromLoc.x][fromLoc.y].y = tileCount - 1;
-    toLoc.x = fromLoc.x;
-    toLoc.y = fromLoc.y;
-    checkSolved();
+  function distance(x1, y1, x2, y2) {
+    return Math.abs(x1 - x2) + Math.abs(y1 - y2);
   }
-}
 
-function checkSolved() {
-  var flag = true;
-  for (var i = 0; i < tileCount; ++i) {
-    for (var j = 0; j < tileCount; ++j) {
-      if (boardParts[i][j].x != i || boardParts[i][j].y != j) {
-        flag = false;
-      }
+  function slideTile(toLoc, fromLoc) {
+    if (!solved) {
+      boardParts[toLoc.x][toLoc.y].x = boardParts[fromLoc.x][fromLoc.y].x;
+      boardParts[toLoc.x][toLoc.y].y = boardParts[fromLoc.x][fromLoc.y].y;
+      boardParts[fromLoc.x][fromLoc.y].x = tileCount - 1;
+      boardParts[fromLoc.x][fromLoc.y].y = tileCount - 1;
+      toLoc.x = fromLoc.x;
+      toLoc.y = fromLoc.y;
+      checkSolved();
     }
   }
-  solved = flag;
+
+  function checkSolved() {
+    var flag = true;
+    for (var i = 0; i < tileCount; ++i) {
+      for (var j = 0; j < tileCount; ++j) {
+        if (boardParts[i][j].x != i || boardParts[i][j].y != j) {
+          flag = false;
+        }
+      }
+    }
+    solved = flag;
+  }
+
 }
